df57f657a5bc2d38998b47bb3e0553b4241aee50
ShreyaBarsude02 <shreyabarsude@gmail.com>
2025-05-14 01:44:41 +0530

    five commit

--- updated-flask-streaming.py
+++ updated-flask-streaming.py
@@ -15,36 +15,42 @@ picam2.start()
 model = YOLO("yolov8n-oiv7.pt")
 
 frame = None
+lock = threading.Lock()
 
 def capture_frames():
     global frame
     while True:
-        frame = picam2.capture_array()
-        results = model(frame)
+        temp_frame = picam2.capture_array()
+        results = model(temp_frame)
 
         for result in results:
-            frame = result.plot()
+            plotted_frame = result.plot()
 
-        # cv2.imshow("Camera - Press 'q' to Quit", frame)
+        with lock:
+            frame = plotted_frame.copy()
+
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
     picam2.close()
     cv2.destroyAllWindows()
-    os._exit(0)  # Stop Flask too
+    os._exit(0)
 
 @app.route('/video_feed')
 def video_feed():
     def generate():
         global frame
         while True:
-            if frame is not None:
-                ret, buffer = cv2.imencode('.jpg', frame)
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+            with lock:
+                if frame is not None:
+                    ret, buffer = cv2.imencode('.jpg', frame)
+                    frame_bytes = buffer.tobytes()
+                else:
+                    continue
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     threading.Thread(target=capture_frames, daemon=True).start()
     app.run(host='0.0.0.0', port=8000)
-
-#http://192.168.0.105:8000/video_feed
